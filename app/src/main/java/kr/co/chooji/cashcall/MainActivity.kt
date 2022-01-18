94c33ab0bac7ac053ef0fd1757ff1dc5c49befe3
eunji <eun7jii3@gmail.com>
2022-01-18 22:52:19 +0900

    feat: 앱 권한 요청

--- app/src/main/java/kr/co/chooji/cashcall/MainActivity.kt
+++ app/src/main/java/kr/co/chooji/cashcall/MainActivity.kt
@@ -2,10 +2,26 @@ package kr.co.chooji.cashcall
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CallLog
+import android.util.Log
+import kr.co.chooji.cashcall.common.Permission
+import java.lang.Exception
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Permission(this)
+    }
+
+    /**
+     * 퍼미션 권한 체크
+     */
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        Permission(requestCode, permissions, grantResults)
     }
 }
---
+++ app/src/main/java/kr/co/chooji/cashcall/common/Permission.kt
@@ -0,0 +1,75 @@
+package kr.co.chooji.cashcall.common
+
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class Permission {
+
+    companion object{
+        const val PERMISSION_CODE = 1
+    }
+
+    private lateinit var activity: Activity
+
+    private val PERMISSIONS = arrayOf(
+        android.Manifest.permission.READ_CALL_LOG
+    )
+
+    constructor(activity: Activity){
+        this.activity = activity
+        reqPermissions()
+    }
+
+    constructor(requestCode: Int, permissions: Array<String>, grantResults: IntArray){
+        onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun hasPermissions(): Boolean{
+        PERMISSIONS.forEach { permission ->
+            if(ContextCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED){
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun reqPermissions(){
+        if(!hasPermissions()){
+            ActivityCompat.requestPermissions(activity, PERMISSIONS, PERMISSION_CODE)
+        }
+    }
+
+    private fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray){
+        if(PERMISSION_CODE == requestCode && PERMISSIONS.size == permissions.size){
+            var checkResult = false
+
+            loop@ for(i in permissions.indices){
+                if(ActivityCompat.checkSelfPermission(activity, permissions[i]) != PackageManager.PERMISSION_GRANTED){
+                    checkResult = true
+                    break@loop
+                }
+            }
+
+            if(checkResult) {
+                var refusal = false
+                loop@ for (i in permissions.indices) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(activity,permissions[i])) {
+                        refusal = true
+                        break@loop
+                    }
+                }
+
+                if (refusal) {
+                    Toast.makeText(activity, "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요.", Toast.LENGTH_SHORT).show()
+                }
+                else {
+                    Toast.makeText(activity, "퍼미션이 거부되었습니다. 설정(애플리케이션 정보)에서 퍼미션을 허용해야 합니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+}
+
